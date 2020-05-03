67ddfd7d8c1125dbdc0c87bd4d1192238da75e42
TOMASBACIGALUPO <tbacigalupo@itba.edu.ar>
2020-05-03 17:47:11 -0300

    adaptation of verlet integration

--- src/main/java/VerletIntegrationParticle.java
+++ src/main/java/VerletIntegrationParticle.java
@@ -1,14 +1,14 @@
 import java.util.List;
 
 public class VerletIntegrationParticle {
-    public static Particle integrate(Particle p, List<Particle> particles, double dt){
+    public static Particle integrate(Particle p, List<Particle> particles, double dt, Vector target){
 
         Vector position = p.position;
         Vector velocity = p.velocity;
         Vector acceleration = p.force.nDiv(p.mass);
         velocity = velocity.nSum(acceleration.nMult(dt/2));
         position = position.nSum(velocity.nMult(dt));
-        //acceleration = p.fr(particles).nDiv(p.mass);
+        acceleration = p.fr(particles,target).nDiv(p.mass);
         velocity = velocity.nSum(acceleration.nMult(dt/2));
 
         return new Particle(position,velocity,p.mass,p.radius);
