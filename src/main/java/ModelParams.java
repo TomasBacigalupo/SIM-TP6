bde9d7e1477e0732c9565749063be2d381977522
lpagn <lpagni@itba.edu.ar>
2020-05-03 18:54:13 -0300

    rmin rmax

--- src/main/java/ModelParams.java
+++ src/main/java/ModelParams.java
@@ -4,8 +4,8 @@ public class ModelParams {
     static double Y_max = 20;
     static double door_width = 1.2;
     static Vector door_pos = new Vector(0,X_max/2);
-    static double r_min = 0.08;
-    static double r_max = 0.09;
+    static double r_min = 0.25;
+    static double r_max = 0.29;
     static double v_min = 0.8;//m/s
     static double v_max = 6;//m/s
     static double A = 2000;//N
