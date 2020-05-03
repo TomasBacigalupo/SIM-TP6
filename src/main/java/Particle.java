31fbb4efb017f13fadfd0f63e97ec1df64f7aba9
TOMASBACIGALUPO <tbacigalupo@itba.edu.ar>
2020-05-03 17:02:21 -0300

    social force

--- src/main/java/Particle.java
+++ src/main/java/Particle.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Particle {
 
+    double radius;
     double mass;
     Vector position;
     Vector velocity;
@@ -11,6 +12,23 @@ public class Particle {
 
 
     public Vector socialForce(List<Particle> influence){
+        Vector force = new Vector(0,0);
+        double Eij = 0;
+        Vector eij;
+        for(Particle p : influence){
+
+            //necesito la distancia a p y versor de direccion
+
+            if(this != p){
+                Eij = this.position.distance(p.position)-this.radius-p.radius;
+                eij = this.position.nRest(p.position).multScalar(1/this.position.distance(p.position));
+                double forceMod = ModelParams.A * Math.exp(-Eij/ModelParams.B);
+                Vector forceVector = eij.multScalar(forceMod);
+                force.sum(forceVector);
+            }
+
+        }
+
         return new Vector(0,0);
     }
 
--- src/main/java/Vector.java
+++ src/main/java/Vector.java
@@ -22,5 +22,25 @@ public class Vector {
     public void setY(double y) {
         this.y = y;
     }
+
+
+    public double distance(Vector otherPoint){
+
+        return Math.sqrt(Math.pow(this.x - otherPoint.x,2)+Math.pow(this.y-otherPoint.y,2));
+
+    }
+
+    public Vector nRest(Vector other){
+        return new Vector(other.x-this.x,other.y-this.y);
+    }
+
+    public Vector multScalar(double scalar ){
+        return new Vector(this.x *scalar,this.y * scalar);
+    }
+
+    public void sum(Vector other){
+        this.x += other.x;
+        this.y += other.y;
+    }
 }
 
