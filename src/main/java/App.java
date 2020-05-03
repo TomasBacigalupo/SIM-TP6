9e7b300eec924c9a0572c279a39e952592070910
TOMASBACIGALUPO <tbacigalupo@itba.edu.ar>
2020-05-03 18:10:42 -0300

    primer intento de main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,11 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
 public class App {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         System.out.println("Social Force Model Simulation");
 
         Room room = new Room();
@@ -12,17 +14,27 @@ public class App {
 
         List<Particle> nParticles = new LinkedList<>();
         StringBuilder sb = new StringBuilder();
-        while(!room.isEmpty()){
+        int i = 0;
+        while(!room.isEmpty() && i < 200){
 
+            sb.append(room.particles.size()).append("\n\n");
             for (Particle p : room.particles) {
                 sb.append(p.toOvito()).append("\n");
-                //TODO: ver si esta afuera o adentro para agregarla
-                nParticles.add(VerletIntegrationParticle.integrate(p,room.particles,ModelParams.dt,ModelParams.target1));
+                if(!room.isInTheDoor(p) && room.isOutside(p)){
+                    Particle np = VerletIntegrationParticle.integrate(p,room.particles,ModelParams.dt,ModelParams.target1);
+                    nParticles.add(np);
+                }
             }
 
             room.particles = nParticles;
-            break;
+            i++;
         }
+
+
+        FileWriter fw = new FileWriter("sim.xyz");
+        fw.write(sb.toString());
+        fw.close();
+
 
     }
 
