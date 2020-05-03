349804f73bca396985493b618b9a0970122e87a6
TOMASBACIGALUPO <tbacigalupo@itba.edu.ar>
2020-05-03 17:59:12 -0300

    armando el main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args){
@@ -7,7 +10,17 @@ public class App {
 
         room.generateParticles();
 
+        List<Particle> nParticles = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
         while(!room.isEmpty()){
+
+            for (Particle p : room.particles) {
+                sb.append(p.toOvito()).append("\n");
+                //TODO: ver si esta afuera o adentro para agregarla
+                nParticles.add(VerletIntegrationParticle.integrate(p,room.particles,ModelParams.dt,ModelParams.target1));
+            }
+
+            room.particles = nParticles;
             break;
         }
 
